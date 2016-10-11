8ae65892626159128cd71617ddac4ccf801fad79
AbuGitHub <abuzar.noorali@hotmail.com>
2016-10-11 16:52:20 -0500

    First commit.

---
+++ HelloWorld_C++/Hello_World.cpp
@@ -0,0 +1,13 @@
+/* Creator: Abuzar Noorali
+   Program Description: Github test program, prints Hello World in C++
+   Date: 10/11/2016
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World! \n";
+	return 0;
+}
